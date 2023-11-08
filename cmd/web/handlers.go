30e495ba8fc1413d16b119465f87d6bce3d79685
OmitNomis <simon98190@gmail.com>
2023-11-08 21:42:05 +0545

    custom error handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,7 +9,7 @@ import (
  
 func (app *application) home(w http.ResponseWriter, r *http.Request){
 	if (r.URL.Path != "/"){
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 	 
@@ -21,15 +21,14 @@ func (app *application) home(w http.ResponseWriter, r *http.Request){
 
 	ts, err := template.ParseFiles(files...) 
 	if (err != nil) {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err)
+		
 		return;
 	}
 	err = ts.Execute(w, nil)
 	
 	if (err != nil){
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err)
 	}
 }
 
@@ -37,7 +36,7 @@ func (app *application) showSnippet (w http.ResponseWriter, r *http.Request){
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 
 	if (err != nil || id <1){
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 	fmt.Fprintln(w, "ID for the snippet is :", id) 
